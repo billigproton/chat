561fec4186c7bb99a0547173cdd061105a72d022
billigproton <danrabien@seznam.cz>
2022-05-23 21:57:11 +0200

    Dovylepseni

--- code.js
+++ code.js
@@ -5,15 +5,18 @@ const zprava1 = document.querySelector('#zprava1');
 const zprava2 = document.querySelector('#zprava2');
 
 let zpravy1 = "";
+let zpravy2 = "";
 
 const chat2 = document.querySelector('#chat2');
 const chat1 = document.querySelector('#chat1');
 
 btnChat1.addEventListener("click", function() {
-    zpravy1 += "Alice:" + zprava1.value + "\n"; 
+    zpravy1 += "Alice: " + zprava1.value + "\n"; 
     chat2.textContent = zpravy1;
+    
 });
 
 btnChat2.addEventListener("click", function() {
-    chat1.textContent = "Bob: " + zprava2.value;
+    zpravy2 += "Bob: " + zprava2.value + "\n";
+    chat1.textContent = zpravy2;
 });
