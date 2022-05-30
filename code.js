df1d375f2535a47e1b1ba129391b376e7590bc1f
billigproton <danrabien@seznam.cz>
2022-05-30 21:33:10 +0200

    přidán prompt na jména a následné zobrazení pod uživateli

--- code.js
+++ code.js
@@ -7,17 +7,24 @@ const zprava2 = document.querySelector('#zprava2');
 let zpravy1 = "";
 let zpravy2 = "";
 
+var uzivatel_1 = prompt("Zadejte jméno prvního uživatele");
+var answer1Element = document.getElementById("Jmeno1")
+answer1Element.innerText = uzivatel_1;
+var uzivatel_2 = prompt("Zadejte jméno druhého uživatele");
+var answer2Element = document.getElementById("Jmeno2")
+answer2Element.innerText = uzivatel_2;
+
 const chat2 = document.querySelector('#chat2');
 const chat1 = document.querySelector('#chat1');
 
 btnChat1.addEventListener("click", function() {
-    zpravy1 += "Alice: " + zprava1.value + "\n"; 
+    zpravy1 += "Uživatel 1: " + zprava1.value + "\n"; 
     chat2.textContent = zpravy1;
     
 });
 
 btnChat2.addEventListener("click", function() {
-    zpravy2 += "Bob: " + zprava2.value + "\n";
+    zpravy2 += "Uživatel 2: " + zprava2.value + "\n";
     chat1.textContent = zpravy2;
 });
 
@@ -35,4 +42,3 @@ person2_img.addEventListener("click", function() {
 
     alert("Jméno: Bob");
 });
-
